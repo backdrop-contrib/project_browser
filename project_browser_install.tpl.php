8ca41a1e75c23344c0632a3a0ad391ad0b299801
Leighton Whiting <wild.katana@gmail.com>
2011-05-31 07:37:14 +0800

    Cleaned up the install page template Added CSS to install page

--- project_browser_install.tpl.php
+++ project_browser_install.tpl.php
@@ -11,20 +11,11 @@
  * @see project_preprocess_project_browser_install()
  */
 ?>
-<?php if ($projects_list) :?>
-  <div class="project-browser">
-  <!-- 
-    <div class="project-browser-header">
-      <?php //print $header; ?>
-    </div>  -->
-    <?php print $list; ?>
-    <?php //print $pager; ?>
-  </div>
-<?php else :?><!-- ENDIF / START ELSE -->
-  <div class="project-browser">
-    <h2><?php print t('No results found.')?></h2>
-  </div>
-<?php endif; ?>
-<div class="project-browser-sidebar-right">
-  <?php print $filter; ?>
+
+<div class="project-browser-install-sidebar-left">
+  <?php print $task_list; ?>
 </div>
+<div class="project-browser-install-main">
+  <?php print $main_content; ?>
+</div>
+
