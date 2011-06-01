8864402974004f477d6da22e8cc0e3eeaba156c0
Leighton Whiting <wild.katana@gmail.com>
2011-06-01 12:10:56 +0800

    Changed some visual and template things to make it look closer to the mockup

--- project_browser_list.tpl.php
+++ project_browser_list.tpl.php
@@ -11,20 +11,25 @@
  * @see project_browser_preprocess_project_browser_list()
  */
 ?>
-<?php if ($projects_list) :?>
-  <div class="project-browser">
-  <!-- 
-    <div class="project-browser-header">
-      <?php //print $header; ?>
-    </div>  -->
-    <?php print $list; ?>
+<div id="dashboard" class="clearfix">
+  <div class="project-browser dashboard-region">
+    <div class='block'>
+      <h2><?php print $title ?></h2>
+      <div class='project-browser-main-content'>
+        <?php print $main_content; ?>
+      </div>
+    </div>
     <?php //print $pager; ?>
   </div>
-<?php else :?><!-- ENDIF / START ELSE -->
-  <div class="project-browser">
-    <h2><?php print t('No results found.')?></h2>
+
+  <div class="project-browser-sidebar-right dashboard-region">
+    <div class="project-browser-filters-block block">
+      <h2>Filters</h2>
+      <?php print $filters; ?>
+    </div>
+    <div class="project-browser-install-queue-block block">
+      <h2>Install list</h2>
+      <?php print $install_list; ?>
+    </div>
   </div>
-<?php endif; ?>
-<div class="project-browser-sidebar-right">
-  <?php print $filter; ?>
 </div>
