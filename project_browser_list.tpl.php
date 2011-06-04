943d6c8d315671b65c2fca56f7767df948027e22
Leighton Whiting <wild.katana@gmail.com>
2011-06-04 17:56:37 +0800

    Changed the theme structure to allow for Contextual Links Themed the blocks and stuff to look like the Dashboard page

--- project_browser_list.tpl.php
+++ project_browser_list.tpl.php
@@ -11,25 +11,15 @@
  * @see project_browser_preprocess_project_browser_list()
  */
 ?>
-<div id="dashboard" class="clearfix">
-  <div class="project-browser dashboard-region">
-    <div class='block'>
-      <h2><?php print $title ?></h2>
-      <div class='project-browser-main-content'>
-        <?php print $main_content; ?>
-      </div>
-    </div>
+<div id="project-browser" class="clearfix">
+  <div id="project-browser-main" class="project-browser-region">
+    <?php print $main_content; ?>
     <?php //print $pager; ?>
   </div>
 
-  <div class="project-browser-sidebar-right dashboard-region">
-    <div class="project-browser-filters-block block">
-      <h2>Filters</h2>
-      <?php print $filters; ?>
-    </div>
-    <div class="project-browser-install-queue-block block">
-      <h2>Install list</h2>
-      <?php print $install_list; ?>
-    </div>
+  <div id="project-browser-sidebar-right" class="project-browser-region">
+    <?php print $filters; ?>
+    <?php print $install_list; ?>
   </div>
+  
 </div>
