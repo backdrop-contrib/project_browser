06d9cbdb370ed756666342255e4a99e17be85c19
Leighton Whiting <wild.katana@gmail.com>
2011-06-10 16:55:52 +0800

    Lots of changes and things added.

--- project_browser_install_queue.tpl.php
+++ project_browser_install_queue.tpl.php
@@ -5,28 +5,8 @@
  * @file
  * 
  * Default theming implementation for displaying list of queued projects
- * 
- * Variables:
- * 
- * $projects
- *   An array of the projects to list
- * 
- * @see project_browser_preprocess_project_browser_list()
  */
 ?>
-<div id="project-browser" class="clearfix">
-  
-  <?php
-  
-  foreach ($projects as $project) {
-    print "<div class='project-browser-install-queue-item' rel='" . $project['name'] . "'>";
-    print "<div class='project_browser-install-queue-item-title'>" . $project['title'] . "</div>";
-    print "</div>";
-  }
-  
-  ?>
-  
-  <div class='project-browser-install-link'>
-    <?php print $install_button; ?>
-  </div>
+<div id="project-browser-install-queue" class="clearfix">
+  <?php print $queue_html; ?>
 </div>
